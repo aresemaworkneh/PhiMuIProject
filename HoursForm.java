104b9c6f26c4d7294387ad3532b875832967b2e5
Aresema W <106709321+aresemaw@users.noreply.github.com>
2023-04-25 11:29:06 -0400

    Update HoursForm.java

--- HoursForm.java
+++ HoursForm.java
@@ -1,4 +1,4 @@
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 public class HoursForm extends JFrame {
     private JTextField hoursField;
     private JTextField qPointsField;
@@ -24,34 +25,45 @@ public class HoursForm extends JFrame {
 
     public HoursForm() {
         setTitle("Hours Form");
-        setSize(300, 150);
+        setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(new JLabel("Hours:"));
+        panel.setBackground(new Color(255, 228, 225)); // Set the background color
+
+        getContentPane().add(panel);
+
+        JLabel hoursLabel = new JLabel("Hours:");
+        hoursLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 14)); // Set the font
+        panel.add(hoursLabel);
         hoursField = new JTextField();
         hoursField.setPreferredSize(new Dimension(150, 25));
         panel.add(hoursField);
 
-        panel.add(Box.createVerticalStrut(15));
+        panel.add(Box.createVerticalStrut(5));
 
-        panel.add(new JLabel("qPoints:"));
+        JLabel qPointsLabel = new JLabel("qPoints:");
+        qPointsLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 14));
+        panel.add(qPointsLabel);
         qPointsField = new JTextField();
         qPointsField.setPreferredSize(new Dimension(150, 25));
         panel.add(qPointsField);
 
-        panel.add(Box.createVerticalStrut(15));
+        panel.add(Box.createVerticalStrut(5));
 
-        panel.add(new JLabel("phiPoints:"));
+        JLabel phiPointsLabel = new JLabel("phiPoints:");
+        phiPointsLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 14));
+        panel.add(phiPointsLabel);
         phiPointsField = new JTextField();
         phiPointsField.setPreferredSize(new Dimension(150, 25));
         panel.add(phiPointsField);
 
-        panel.add(Box.createVerticalStrut(15));
+        panel.add(Box.createVerticalStrut(10));
 
         // Add a "Submit" button to submit the form
         JButton submitButton = new JButton("Submit");
+        submitButton.setFont(new Font("Bookman Old Style", Font.PLAIN, 14));
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -81,4 +93,3 @@ public class HoursForm extends JFrame {
         hoursForm.setVisible(true);
     }
 }
-
